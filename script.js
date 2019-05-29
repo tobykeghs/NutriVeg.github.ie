cda0d4097e07c3f30f9ac29dcff805a9be6bf573
tobykeghs <51098247+tobykeghs@users.noreply.github.com>
2019-05-29 04:15:14 +0100

    portfolio

---
+++ script.js
@@ -0,0 +1,14 @@
+AOS.init({
+    duration: 1200,
+})
+
+$(document).ready(function(){
+    $('.content-main3').slick({
+        mobileFirst: true,
+        fade:true,
+        autoplay:true,
+        autoplaySpeed: 3000,
+        dots:false,
+  
+ });
+ });
